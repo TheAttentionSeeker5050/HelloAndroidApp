9851af89ec76d478455a4bdb11666da56316ab3e
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-28 19:57:35 -0400

    added basic layout

---
+++ app/src/main/java/com/example/myhelloapp/HelloApp.kt
@@ -0,0 +1,123 @@
+package com.example.myhelloapp
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myhelloapp.ui.theme.MyHelloAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+// This app holds the main container
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyHelloApp() {
+    // make the state variable myname
+    var myname by remember { mutableStateOf(TextFieldValue("Nick")) }
+    var message by remember { mutableStateOf("Hello, ${myname.text}") }
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource( id = R.string.app_name )) },
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        
+    ) {innerPadding ->
+        Box(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp, 8.dp),
+                verticalArrangement = Arrangement.Top,
+
+
+                ) {
+                Text(
+                    text = stringResource(
+                        id = R.string.insert_name_message
+                    ),
+                    modifier = Modifier.padding(0.dp, 8.dp)
+                )
+
+                TextField(
+                    modifier = Modifier.padding(0.dp, 8.dp),
+                    value = myname,
+                    onValueChange = { newName -> myname = newName },
+                )
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Say Hi")
+                }
+
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxSize(0.6F)
+                    ,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+
+                    Image(
+                        painter = painterResource(
+                            id = R.drawable.person_saying_hi_nobg
+                        ),
+                        contentDescription = "Person Saying Hi",
+                        modifier = Modifier
+                            .fillMaxSize(0.55F)
+                    )
+
+                    Text(
+                        text = message
+                    )
+
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Say Goodbye")
+                    }
+
+                }
+
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyHelloAppPreview() {
+    MyHelloAppTheme {
+        MyHelloApp()
+    }
+}
